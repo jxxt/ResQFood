620b98271b2a1d5a51f9b72200d10c7a70b71db0
Jeet Debnath <jeet.debnath2004@gmail.com>
2024-03-31 21:35:48 +0530

    added backend and styles

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const path = require('path');
 const mysql = require('mysql');
-const bodyParser = require('body-parser'); // Import body-parser module
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = 3000;
 
-// MySQL connection
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
@@ -14,7 +13,6 @@ const db = mysql.createConnection({
     database: 'cred'
 });
 
-// Connect to MySQL
 db.connect((err) => {
     if (err) {
         throw err;
@@ -22,18 +20,14 @@ db.connect((err) => {
     console.log('Connected to MySQL database');
 });
 
-// Use body-parser middleware to parse request bodies
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Serve static files from the public directory (optional)
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Define a route handler for the root URL ("/") to serve index.html
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// Handle user login
 app.post('/login', (req, res) => {
     const { username, password } = req.body;
     const query = 'SELECT * FROM test WHERE username = ? AND password = ?';
@@ -52,9 +46,9 @@ app.post('/login', (req, res) => {
 });
 
 app.post('/signup', (req, res) => {
-    const { username, password } = req.body;
-    const query = 'insert into test values (?,?)';
-    db.query(query, [username, password], (err, result) => {
+    const { username, password, fullname, email } = req.body;
+    const query = 'insert into test values (?,?,?,?)';
+    db.query(query, [username, password, fullname, email], (err, result) => {
         if (err) {
             res.status(500).send('Error while fetching user');
             return;
@@ -68,7 +62,44 @@ app.post('/signup', (req, res) => {
     });
 });
 
-// Start the server
+app.post('/contactus', (req, res) => {
+    const { firstname, lastname, email, phone, message } = req.body;
+    const query = 'insert into contact_form_info values (?,?,?,?,?)';
+    db.query(query, [firstname, lastname, email, phone, message], (err, result) => {
+        if (err) {
+            res.status(500).send('Error while fetching user');
+            return;
+        }
+        if (result.length === 0) {
+            res.status(401).send('enter something');
+        } else {
+            // res.status(200).send('Login successful');
+            res.redirect('./thankyou.html');
+        }
+    });
+});
+
+app.post('/feedback', (req, res) => {
+    const { q1, q2, q3, q4, q5, q6, q7, q8, q9, q10, comment } = req.body;
+    const query = 'insert into feedback_form values (?,?,?,?,?,?,?,?,?,?,?)';
+    db.query(query, [q1, q2, q3, q4, q5, q6, q7, q8, q9, q10, comment], (err, result) => {
+        if (err) {
+            res.status(500).send('Error while fetching user');
+            return;
+        }
+        if (result.length === 0) {
+            res.status(401).send('enter something');
+        } else {
+            // res.status(200).send('Login successful');
+            res.redirect('./thankyou.html');
+        }
+    });
+});
+
+
+
+
+
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
